38c0b4a193ec487d288ee21466fb3f1955a7b97a
yuryoinn <yuryoinn88@gmail.com>
2022-11-22 16:44:31 +0900

    opening

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+const remainTime = document.querySelector('#remain-time');
+
+function dDay() {
+    const openTime = new Date("2022-12-14");
+    const todayTime = new Date();
+
+    const dday = openTime - todayTime;
+
+    const ddayDay = Math.floor(dday / (1000*60*60*24));
+    const ddayHour = Math.floor((dday / (1000*60*60)) % 24);
+    const ddayMin = Math.floor((dday / (1000*60)) % 60);
+    const ddaySec = Math.floor(dday / 1000 % 60);
+
+    remainTime.innerText = `${ddayDay}일 ${ddayHour}시간 ${ddayMin}분 ${ddaySec}초`;
+}
+
+dDay();
+setInterval(dDay, 1000);
